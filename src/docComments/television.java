cc51b480c43b5b35062257670a465019a3315e9a
Michael Evans <evansrichardmichael@gmail.com>
2024-08-27 20:04:32 -0600

    starting stuff

--- src/docComments/television.java
+++ src/docComments/television.java
@@ -1,5 +1,46 @@
 package docComments;
 
 public class television {
+private int ID;
+private int count;
+private int resolution;
+private int refreshRate;
+private int dimensions;
 
+
+
+public television(int iD, int count, int resolution, int refreshRate, int dimensions) {
+	
+	this.ID = iD;
+	this.resolution = resolution;
+	this.refreshRate = refreshRate;
+	this.dimensions = dimensions;
+}
+
+public int getResolution() {
+	return resolution;
+}
+public void setResolution(int resolution) {
+	this.resolution = resolution;
+}
+public int getRefreshRate() {
+	return refreshRate;
+}
+public void setRefreshRate(int refreshRate) {
+	this.refreshRate = refreshRate;
+}
+public int getDimensions() {
+	return dimensions;
+}
+public void setDimensions(int dimensions) {
+	this.dimensions = dimensions;
+}
+
+public int getCount() {
+	return count;
+}
+
+public void setCount(int count) {
+	this.count = count;
+}
 }
